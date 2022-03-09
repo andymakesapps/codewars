70188e724707509104e0fd4cd410d96b04e34115
Andrei Gavrila <andreigavrila@andreis-mbp.home>
2022-03-09 22:30:06 +0000

    Added solution for polynomial

--- Python/Simplifying multilinear polynomials/solution.py
+++ Python/Simplifying multilinear polynomials/solution.py
@@ -1,13 +1,36 @@
-from numpy import poly1d
-
-
 import re
+
 def simplify(poly):
-    pattern = r'([+\-]?)(\d*)([a-z]+)'
-    matches = re.findall(pattern, poly)
-    for match in matches:
-        if match[1] == '':
-            match[1] = '1'
+    poly_dict = {}
+    print(poly)
+    pattern = r'[-]?[\d+]?\w+'
+    poly_matches = re.findall(pattern, poly)
+    print(poly_matches)
+    for match in poly_matches:
+        match_list = break_up_poly(match)
+        match_symbol = match_list[0]
+        match_unit = match_list[1]
+        if match_symbol in poly_dict:
+            poly_dict[match_symbol] = poly_dict[match_symbol] + match_unit
+        else:
+            poly_dict[match_symbol] = match_unit
+    print(poly_dict)
+
+def break_up_poly(poly_unit):
+    unit_value = 1
+    if poly_unit[0] == '-':
+        unit_value = -1
+    poly_unit = poly_unit[1:]
+    
+    pattern = '([\d+]?)(\w+)'
+    poly_matches = re.match(pattern, poly_unit)
+    poly_matches_list = list((poly_matches.groups()))
+    print(poly_matches_list)
+    if poly_matches_list[0] == '':
+        poly_matches_list[0] = '1'
+    unit_symbol = "".join(sorted(poly_matches_list[1]))
+    unit_value = unit_value * int(poly_matches_list[0])
+    return [unit_symbol, unit_value]
 
 def main():
     poly1 = "dc+dcba"
