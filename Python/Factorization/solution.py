b283718ccbf9f362db1ebde25fc3a3e324b0cf0e
Andrei Gavrila <andreigavrila@andreis-mbp.home>
2022-04-03 21:41:28 +0100

    Added factorize challenge

---
+++ Python/Factorization/solution.py
@@ -0,0 +1,22 @@
+from math import factorial
+
+
+def factorize(number):
+    factors = []
+    output = ''
+    i = 2
+    while number != 1:
+        if number % i == 0:
+            factors.append(i)
+            number = number / i
+        else:
+            i += 1
+    for item in list(set(factors)):
+        output += '({}**{})'.format(str(item), str(factors.count(item)))
+    print(output)
+
+def main():
+    factorize(480)
+
+if __name__ == '__main__':
+    main()
